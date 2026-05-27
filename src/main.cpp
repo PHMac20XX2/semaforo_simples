eebca6c15e505c932645407256db1b0375a01da2
Pedro Henrique Macedo de Santana <psantana@newip.com.br>
2026-05-27 14:04:37 -0300

    att codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  delay(1500); // Wait for 1500 millisecond(s)
+  digitalWrite(12, LOW);
+  digitalWrite(11, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
 }
